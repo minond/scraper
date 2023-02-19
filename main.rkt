3e658d5b66a48f57dbfae8dd52545f6e3f586d19
Marcos Minond <minond.marcos@gmail.com>
2023-02-18 18:49:22 -0700

    Workign on find-root function

--- main.rkt
+++ main.rkt
@@ -13,6 +13,15 @@
          (prefix-in h: html)
          (prefix-in x: xml))
 
+(struct article (sections))
+(struct section (children))
+(struct heading (text))
+(struct paragraph (content))
+(struct lista (items))
+(struct text (text))
+(struct image (src))
+(struct link (url))
+
 (define (page-document url)
   (let* ([res (get (string->url url))]
          [bod (http-response-body res)]
@@ -72,41 +81,54 @@
 ;         [else
 ;          acc])))
 
-(define (find-elements tag elem [acc null])
+(define (find-elements tag root [acc null])
   (cond
-    [(list? elem)
-     (append (flatten (map (curry find-elements tag) elem))
+    [(list? root)
+     (append (flatten (map (curry find-elements tag) root))
              acc)]
-    [(x:element? elem)
-     (if (eq? tag (x:element-name elem))
-         (cons elem acc)
-         (append (flatten (map (curry find-elements tag) (x:element-content elem)))
+    [(x:element? root)
+     (if (eq? tag (x:element-name root))
+         (cons root acc)
+         (append (flatten (map (curry find-elements tag) (x:element-content root)))
                  acc))]
     [else acc]))
 
-(define (element-string elem [acc ""])
-  (if (x:pcdata? elem)
-      (string-append acc (x:pcdata-string elem))
-      (~>> (x:element-content elem)
-           (map element-string)
-           (apply string-append)
-           (string-append acc)
-           (regexp-replace* #px"^\\s+|\\s+$" _ ""))))
+(define (find-element tag root)
+  (let ([res (find-elements tag elem)])
+    (if (not (empty? res))
+        (car res)
+        #f)))
 
-; (define (filter-elements query elems)
-;   (flatten
-;    (filter (negate empty?)
-;            (map (lambda (elem)
-;                   (se-path*/list query elem)) elems))))
+(define (find-article-root doc)
+  (let* ([body (find-element 'body doc)])
+    body))
+
+(define (element-string elem [acc ""])
+  (cond
+    [(x:entity? elem) acc]
+    [(x:pcdata? elem)
+     (string-append acc (x:pcdata-string elem))]
+    [else
+     (~>> (x:element-content elem)
+          (map element-string)
+          (apply string-append)
+          (string-append acc)
+          (regexp-replace* #px"^\\s+|\\s+$" _ ""))]))
+
+
 
 (define elem (page-document "https://shopify.engineering/scale-performance-testing"))
+(define main (car (find-elements 'main elem)))
 
-(define title (car (find-elements 'title elem)))
+; (define title (car (find-elements 'title elem)))
+;
+; (for ([p (find-elements 'p main)])
+;   (displayln (element-string p)))
 
-(displayln (find-elements 'p elem))
+; (displayln (element-string main))
 
-title
-(printf "\n(element-string title) => \"~a\"\n" (element-string title))
+; title
+; (printf "(element-string title) => \"~a\"\n" (element-string title))
 
 ; (displayln elem)
 
