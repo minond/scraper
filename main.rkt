c13124eb6d335368517260421a556a5efd24fec6
Marcos Minond <minond.marcos@gmail.com>
2023-02-25 18:38:14 -0700

    Cleaning up dead code and grouping functions by usage

--- main.rkt
+++ main.rkt
@@ -14,22 +14,6 @@
          (prefix-in h: html)
          (prefix-in x: xml))
 
-(struct article (sections))
-(struct section (children))
-(struct heading (text))
-(struct paragraph (content))
-(struct lista (items))
-(struct text (text))
-(struct image (src))
-(struct link (url))
-
-(define (page-document url)
-  (let* ([res (get (string->url url))]
-         [bod (http-response-body res)]
-         [doc (h:read-html-as-xml (open-input-string bod))])
-    ; [doc (h:read-html (open-input-string bod))])
-    doc))
-
 (define (/content req)
   (let* ([url (request-binding-assoc 'url req)]
          [doc (page-document url)])
@@ -57,76 +41,9 @@
 
 ; (start/servlet)
 
-; (define (filter-elements pred elem [acc null])
-;   ; (displayln "=============")
-;   ; (displayln (pred elem))
-;   (if (pred elem)
-;       (cons elem acc)
-;       (match elem
-;         [(struct h:html-full (_ children))
-;          ; (for ([elem children])
-;          ;   (displayln elem))
-;          (flatten (map (lambda (elem) (filter-elements pred elem acc)) children))]
-;         ; (append (flatten (map (lambda (elem)
-;         ;                         (filter-elements pred elem acc)) children)) acc)]
-;         ; (map (lambda (elem)
-;         ;        (filter-elements pred elem acc)) children)]
-;
-;         ; (for ([elem children])
-;         ;   (displayln elem))
-;         ; ]
-;
-;         [(struct h:html-element _) acc]
-;         ; [(struct x:pcdata _) acc]
-;
-;         [else
-;          acc])))
-
-(define (find-elements tag root [acc null])
-  (cond
-    [(list? root)
-     (append (flatten (map (curry find-elements tag) root))
-             acc)]
-    [(x:element? root)
-     (if (eq? tag (x:element-name root))
-         (cons root acc)
-         (append (flatten (map (curry find-elements tag) (x:element-content root)))
-                 acc))]
-    [else acc]))
-
-(define (find-element tag root)
-  (let ([res (find-elements tag root)])
-    (if (not (empty? res))
-        (car res)
-        #f)))
-
-(define (find-article-root doc)
-  (let* ([body (find-element 'body doc)]
-         [tran (transform body)])
-    (find-highest-score (transform body))))
-
-
-(define (element-string elem [acc ""])
-  (cond
-    [(x:entity? elem) acc]
-    [(x:pcdata? elem)
-     (string-append acc (x:pcdata-string elem))]
-    [else
-     (~>> (x:element-content elem)
-          (map element-string)
-          (apply string-append)
-          (string-append acc)
-          (regexp-replace* #px"^\\s+|\\s+$" _ ""))]))
-
-
-
-(define doc (page-document "https://shopify.engineering/scale-performance-testing"))
-(define body (find-element 'body doc))
-
-; (displayln (x:element-name (find-article-root doc)))
-
-; (define title (car (find-elements 'title doc)))
-;
+(struct element (tag score percentage children ref)
+  #:transparent
+  #:mutable)
 
 (define p-worth (make-parameter 10))
 (define lista-worth (make-parameter 4))
@@ -152,34 +69,23 @@
               (return (worth))))))
       0)))
 
-(define (show elem [level 0])
-  (let ([el (element-ref elem)]
-        [padding (string-join (make-list level " "))])
-    (cond
-      [(x:element? el)
-       (printf "~a~a (~a ~a%) {\n" padding (element-tag elem) (element-score elem) (element-percentage elem))
-       (for ([ch (element-children elem)])
-         (show ch (add1 level)))
-       (printf "~a}\n" padding)]
-      [else
-       (printf "~a~a (0%)\n" padding (object-name el))])))
+(define (page-document url)
+  (let* ([res (get (string->url url))]
+         [bod (http-response-body res)]
+         [doc (h:read-html-as-xml (open-input-string bod))])
+    doc))
 
-(define (find-highest-score elem)
-  (let* ([root
-          (if (zero? (element-percentage elem))
-              (argmax element-score (element-children elem))
-              elem)]
-         [next
-          (and root
-               (findf (lambda (elem)
-                        (> (element-percentage elem) 80))
-                      (element-children root)))])
-    (or (and next (find-highest-score next))
-        (or root elem))))
-
-(struct element (tag score percentage children ref)
-  #:transparent
-  #:mutable)
+(define (element-string elem [acc ""])
+  (cond
+    [(x:entity? elem) acc]
+    [(x:pcdata? elem)
+     (string-append acc (x:pcdata-string elem))]
+    [else
+     (~>> (x:element-content elem)
+          (map element-string)
+          (apply string-append)
+          (string-append acc)
+          (regexp-replace* #px"^\\s+|\\s+$" _ ""))]))
 
 (define (transform el)
   (cond
@@ -207,31 +113,62 @@
     [else
      (element (object-name el) 0 0 null el)]))
 
-; (pretty-display (transform body))
+(define (find-elements tag root [acc null])
+  (cond
+    [(list? root)
+     (append (flatten (map (curry find-elements tag) root))
+             acc)]
+    [(x:element? root)
+     (if (eq? tag (x:element-name root))
+         (cons root acc)
+         (append (flatten (map (curry find-elements tag) (x:element-content root)))
+                 acc))]
+    [else acc]))
+
+(define (find-element tag root)
+  (let ([res (find-elements tag root)])
+    (if (not (empty? res))
+        (car res)
+        #f)))
+
+(define (find-article-root doc)
+  (let* ([body (find-element 'body doc)]
+         [tran (transform body)])
+    (find-highest-score (transform body))))
+
+(define (find-highest-score elem)
+  (let* ([root
+          (if (zero? (element-percentage elem))
+              (argmax element-score (element-children elem))
+              elem)]
+         [next
+          (and root
+               (findf (lambda (elem)
+                        (> (element-percentage elem) 80))
+                      (element-children root)))])
+    (or (and next (find-highest-score next))
+        (or root elem))))
+
+(define (show elem [level 0])
+  (let ([el (element-ref elem)]
+        [padding (string-join (make-list level " "))])
+    (cond
+      [(x:element? el)
+       (printf "~a~a (~a ~a%) {\n" padding (element-tag elem) (element-score elem) (element-percentage elem))
+       (for ([ch (element-children elem)])
+         (show ch (add1 level)))
+       (printf "~a}\n" padding)]
+      [else
+       (printf "~a~a (0%)\n" padding (object-name el))])))
+
+
+
+(define doc (page-document "https://shopify.engineering/scale-performance-testing"))
+(define body (find-element 'body doc))
+
 (show (find-article-root doc))
 
 (with-output-to-file "ignore.txt" #:exists 'replace
   (lambda ()
     (show (find-article-root doc))
     (show (transform body))))
-
-
-; (displayln (element-string main))
-
-; title
-; (printf "(element-string title) => \"~a\"\n" (element-string title))
-
-; (displayln elem)
-
-; (displayln (filter-elements h:title? elem))
-; (define divs (filter-elements h:div? elem))
-; (displayln (filter-elements h:article? elem))
-
-; ; (displayln (filter-elements '(head title) elem))
-; (let* ([elems (filter-elements '(article) elem)])
-;   (for ([elem elems
-;
-; ; (displayln (cdddr elem))
-;
-; ; (displayln elem)
-; ; (displayln (h:html-full-content (filter-elements h:title? elem)))
